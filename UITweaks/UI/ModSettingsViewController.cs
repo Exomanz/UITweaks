ad6ff534a459a3b781ea368cb4025635ee3075b7
Exomanz <trusch102102@gmail.com>
2024-07-11 23:04:31 -0500

    Check `gameObject` field instead of type check

--- UITweaks/UI/ModSettingsViewController.cs
+++ UITweaks/UI/ModSettingsViewController.cs
@@ -66,7 +66,7 @@ namespace UITweaks.UI
         protected override void DidActivate(bool firstActivation, bool addedToHierarchy, bool screenSystemEnabling)
         {
             base.DidActivate(firstActivation, addedToHierarchy, screenSystemEnabling);
-            if (firstActivation && rainbowSpeedSetting != null)
+            if (firstActivation && rainbowSpeedSetting.gameObject != null)
             {
                 rainbowSpeedSetting.slider.valueDidChangeEvent += HandleRainbowSliderValueChanged;
             }
